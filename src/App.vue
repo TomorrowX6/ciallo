979670a2003f5e73575269b68322ff9dd10b3fc8
TomorrowX6 <454565615@qq.com>
2025-05-27 19:15:08 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <h1>喵喵</h1>
+    <md-filled-button @click="playSound">点我</md-filled-button>
+  </main>
+</template>
+
+<script setup>
+import '@material/web/button/filled-button.js'
+
+const audio = new Audio('src/sounds/click.mp3')
+
+function playSound() {
+  audio.currentTime = 0
+  audio.play()
+}
+</script>
+
+<style scoped>
+md-filled-button {
+  margin-top: 16px;
+  --md-sys-color-primary: #1a73e8;
+}
+</style>
